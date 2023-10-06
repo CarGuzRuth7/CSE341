52d394512e555599a200c15b5b42eb84525de96a
CarGuzRuth7 <cardonaguzmanruth97@gmail.com>
2023-10-06 16:59:32 -0300

    Lesson4: Add swagger API documentation

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const routes = require('express').Router();
 //const l1Controller = require('../controllers/index');
 //const l2Contacts = require('./contacts')
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 // routes.get('/', l1Controller.matiasRoute);
 // routes.get('/linda', l1Controller.lindaRoute);
@@ -9,5 +11,7 @@ const routes = require('express').Router();
 // routes.get('/contacts/:id', l2Contacts.getContactById);
 
 routes.use('/contacts', require('./contacts'));
+routes.use('/api-docs', swaggerUi.serve);
+routes.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 module.exports = routes;

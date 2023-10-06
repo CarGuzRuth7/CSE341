9829851b1a543a28ca7502412966721be05bb0d5
CarGuzRuth7 <cardonaguzmanruth97@gmail.com>
2023-10-06 17:19:02 -0300

    Modify swagger files

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,13 @@
 const routes = require('express').Router();
 //const l1Controller = require('../controllers/index');
 //const l2Contacts = require('./contacts')
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
 
 // routes.get('/', l1Controller.matiasRoute);
 // routes.get('/linda', l1Controller.lindaRoute);
 // routes.get('/virginia', l1Controller.virginiaRoute);
 // routes.get('/contacts', l2Contacts.getAllContacts);
 // routes.get('/contacts/:id', l2Contacts.getContactById);
-
+routes.use('/', require('./swagger'));
 routes.use('/contacts', require('./contacts'));
-routes.use('/api-docs', swaggerUi.serve);
-routes.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 module.exports = routes;
--- routes/swagger.js
+++ routes/swagger.js
@@ -1,19 +1,8 @@
-const swaggerAutogen = require('swagger-autogen')();
+const routes = require('express').Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
 
-const doc = {
-  info: {
-    title: 'My API',
-    description: 'Description'
-  },
-  host: 'localhost:3000',
-  schemes: ['http']
-};
+routes.use('/api-docs', swaggerUi.serve);
+routes.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
-const outputFile = './swagger.json';
-const endpointsFiles = ['./routes/index.js'];
-
-/* NOTE: if you use the express Router, you must pass in the 
-   'endpointsFiles' only the root file where the route starts,
-   such as index.js, app.js, routes.js, ... */
-
-swaggerAutogen(outputFile, endpointsFiles, doc);
+module.exports = routes;

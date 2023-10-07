a8c6fabe292ae7cb1d85eba6b587f58e24ad18f4
CarGuzRuth7 <cardonaguzmanruth97@gmail.com>
2023-10-07 16:03:45 -0300

    modify typos

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const port = process.env.PORT || 3000;
 app
   .use(cors())
   .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'),
-    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
   })
   .use(express.json())
